c5deac5fded86b6c7143eb476216424cd88edadf
hiteshdev115 <hitesh1988.ca@gmail.com>
2026-04-05 15:27:40 -0400

    pipeline initiated

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "cartzii-qa",
+      script: "server.js",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+        HOSTNAME: "0.0.0.0",
+      },
+    },
+  ],
+};
