aa09b74f92f0f3f12a514345e2d37c377177d771
hiteshdev115 <hitesh1988.ca@gmail.com>
2026-04-07 16:01:32 -0400

    config changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,8 @@ module.exports = {
         NODE_ENV: "production",
         PORT: 3003,
         HOSTNAME: "0.0.0.0",
+        NEXT_PUBLIC_API_URL: "https://staging-api.cartzii.com",
+        NEXT_PUBLIC_GUEST_API_TOKEN: "f6ec111259e3d0f10af32ec3724b82285866aa2af516b0ebdbd2ce4d54d39467",
       },
     },
   ],
